dae2ea317b9f48d203fe22cd5d59661c68f87b6f
orangejack <994133136@qq.com>
2019-12-24 15:18:01 +0800

    change package

---
+++ src/chapter2_Sort/Insertion.java
@@ -0,0 +1,22 @@
+package chapter2_Sort;
+
+/**
+ * @Description:插入排序
+ * @Create: 2019-12-24-14:59
+ */
+public class Insertion {
+
+    public static void sort(Comparable[] a){
+        for (int i = 1; i < a.length; i++) {
+            for (int j = i; j > 0 && Example.less(a[j],a[j-1]); j--)
+                Example.exch(a,j,j-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"bed","yes","zoo","abc","dad"};
+        sort(a);
+        assert Example.isSorted(a);
+        Example.show(a);
+    }
+}
